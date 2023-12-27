49142b8621be098fd8a747dda0468cf6a3c51dfa
Adnene Guessoum <adnen.guessoum@gmail.com>
2023-12-27 22:55:51 +0100

    fix: remove magic number using #define

--- 1_ch_tutorial/temp_conv/for_loop/conversion.c
+++ 1_ch_tutorial/temp_conv/for_loop/conversion.c
@@ -2,10 +2,16 @@
 
 /* print fahr to celsius table using for loop */
 
+/* using symbolic constants */
+
+#define LOWER 0             /* lower limit of the conversion table */
+#define UPPER 300           /* upper limit of the conversion table */
+#define STEP 20             /* step size */
+
 main() {
 	int fahr;
 
-	for (fahr = 300; fahr >= 0; fahr = fahr - 20) {
+	for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP) {
 		printf("%3d %6.1f \n", fahr, (5.0/9.0)*(fahr-32));
 	}
 }
